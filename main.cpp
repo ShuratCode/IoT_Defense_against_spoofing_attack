77d35f963693947224383fb5e9f90ae81c3be2ad
Shaked Eyal <shaked.eyal@gmail.com>
2018-10-21 10:14:26 +0300

    Added Event queue

--- main.cpp
+++ main.cpp
@@ -21,16 +21,16 @@ void checkData();
 
 
 int main() {
-    /* Local variables declartion */
+    /* Local variables declaration */
     int16_t pDataXYZ[3] = {0};
     float pGyroDataXYZ[3] = {0};
     int interval = 0;
-    //magnometer buffers
+    //magnetometer buffers
     CircularBuffer<int16_t, BUFFER_SIZE> magnoXBuf;
     CircularBuffer<int16_t, BUFFER_SIZE> magnoYBuf;
     CircularBuffer<int16_t, BUFFER_SIZE> magnoZBuf;
 
-    //accelometer buffers
+    //accelerometer buffers
     CircularBuffer<int16_t, BUFFER_SIZE> acceloXBuf;
     CircularBuffer<int16_t, BUFFER_SIZE> acceloYBuf;
     CircularBuffer<int16_t, BUFFER_SIZE> acceloZBuf;
@@ -54,26 +54,17 @@ int main() {
             led = 0;
         } else { //read the sensors and fill the buffer queue
             BSP_MAGNETO_GetXYZ(pDataXYZ);
-            /*printf("\nMAGNETO_X = %d\n", pDataXYZ[0]);
-            printf("MAGNETO_Y = %d\n", pDataXYZ[1]);
-            printf("MAGNETO_Z = %d\n", pDataXYZ[2]);*/
             magnoXBuf.push(pDataXYZ[0]);
             magnoYBuf.push(pDataXYZ[1]);
             magnoZBuf.push(pDataXYZ[2]);
            
             
             BSP_GYRO_GetXYZ(pGyroDataXYZ);
-           /* printf("\nGYRO_X = %.2f\n", pGyroDataXYZ[0]);
-            printf("GYRO_Y = %.2f\n", pGyroDataXYZ[1]);
-            printf("GYRO_Z = %.2f\n", pGyroDataXYZ[2]);*/
             gyroXBuf.push(pGyroDataXYZ[0]);
             gyroYBuf.push(pGyroDataXYZ[1]);
             gyroZBuf.push(pGyroDataXYZ[2]);
             
             BSP_ACCELERO_AccGetXYZ(pDataXYZ);
-            /*printf("\nACCELERO_X = %d\n", pDataXYZ[0]);
-            printf("ACCELERO_Y = %d\n", pDataXYZ[1]);
-            printf("ACCELERO_Z = %d\n", pDataXYZ[2]);*/
             acceloXBuf.push(pDataXYZ[0]);
             acceloYBuf.push(pDataXYZ[1]);
             acceloZBuf.push(pDataXYZ[2]);
