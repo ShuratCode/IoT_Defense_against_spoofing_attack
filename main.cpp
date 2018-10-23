7dcdff6b8a1f3a10659035a446aa0a4ac3df4a3d
Shaked Eyal <shaked.eyal@gmail.com>
2018-10-23 14:41:06 +0300

    Cleanup code and added documentation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "stm32l475e_iot01_magneto.h"
 #include "stm32l475e_iot01_gyro.h"
 #include "stm32l475e_iot01_accelero.h"
-#include "Servo.h"
 
 
 DigitalOut led(LED1);
@@ -22,30 +21,31 @@ void controlServo(float pGyroDataXYZ[3]);
 void read_magnetometer();
 void read_gyro_accelerometer();
 
- /* Local variables declaration */
-    int16_t pDataXYZ[3] = {0};
-    float pGyroDataXYZ[3] = {0};
+/* Global variables declaration */
+int16_t pDataXYZ[3] = {0};
+float pGyroDataXYZ[3] = {0};
+    
+//magnetometer buffers
+CircularBuffer<int16_t, BUFFER_SIZE> magnoXBuf;
+CircularBuffer<int16_t, BUFFER_SIZE> magnoYBuf;
+CircularBuffer<int16_t, BUFFER_SIZE> magnoZBuf;
 
-    //magnetometer buffers
-    CircularBuffer<int16_t, BUFFER_SIZE> magnoXBuf;
-    CircularBuffer<int16_t, BUFFER_SIZE> magnoYBuf;
-    CircularBuffer<int16_t, BUFFER_SIZE> magnoZBuf;
+//accelerometer buffers
+CircularBuffer<int16_t, BUFFER_SIZE> acceloXBuf;
+CircularBuffer<int16_t, BUFFER_SIZE> acceloYBuf;
+CircularBuffer<int16_t, BUFFER_SIZE> acceloZBuf;
 
-    //accelerometer buffers
-    CircularBuffer<int16_t, BUFFER_SIZE> acceloXBuf;
-    CircularBuffer<int16_t, BUFFER_SIZE> acceloYBuf;
-    CircularBuffer<int16_t, BUFFER_SIZE> acceloZBuf;
+//gyro buffers
+CircularBuffer<float, BUFFER_SIZE> gyroXBuf;
+CircularBuffer<float, BUFFER_SIZE> gyroYBuf;
+CircularBuffer<float, BUFFER_SIZE> gyroZBuf;
 
-    //gyro buffers
-    CircularBuffer<float, BUFFER_SIZE> gyroXBuf;
-    CircularBuffer<float, BUFFER_SIZE> gyroYBuf;
-    CircularBuffer<float, BUFFER_SIZE> gyroZBuf;
+//Configure PwmOut
+PwmOut _pwm(D0);
 
-    //Configure Servo motor
-    // Servo servo(D0);
-    PwmOut _pwm(D0);
-
-
+/**
+ * Main function. Initialize the sensors and set up the event queue
+ */
 int main() {
    
     //Initiate the sensors
@@ -53,26 +53,34 @@ int main() {
     BSP_GYRO_Init();
     BSP_ACCELERO_Init();
 
-    //Configure eventqueue to run checkData() every 20ms
+    //Configure eventqueue 
     EventQueue queue;
     queue.call_every(20, checkData);
     queue.call_every(25, read_magnetometer);
     queue.call_every(80, read_gyro_accelerometer);
     queue.dispatch(-1);
-
-    
-
 }
 
+/**
+ * Check the buffers and decide if we have an attack
+ */ 
 void checkData() {
-    //printf("\n\n\n THIS IS CHECK DATA \n\n\n");
+    //TODO: Completes
 }
 
+/**
+ * Give the data that had been collected from the gyro
+ * and use it to control the servo engine
+ * @param pGyroDataXYZ array of the data from the gyro. On 3 axes
+ */
 void controlServo(float pGyroDataXYZ[3]){
      float xGyro = pGyroDataXYZ[0];
      _pwm.pulsewidth_us(1500+xGyro/1000.0);
 }
 
+/**
+ * Read the data from the magnetometer
+ */
 void read_magnetometer(){
      BSP_MAGNETO_GetXYZ(pDataXYZ);
         magnoXBuf.push(pDataXYZ[0]);
@@ -80,6 +88,9 @@ void read_magnetometer(){
         magnoZBuf.push(pDataXYZ[2]);
 }
 
+/**
+ * Read the data from the accelerometer and from the gyro
+ */
 void read_gyro_accelerometer(){
     BSP_ACCELERO_AccGetXYZ(pDataXYZ);
         acceloXBuf.push(pDataXYZ[0]);
