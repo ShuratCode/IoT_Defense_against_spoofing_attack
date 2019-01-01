7b8a94aa54d0e89d2d59f5dc869e3b9aca2abef0
Shaked Eyal <shaked.eyal@gmail.com>
2019-01-01 12:24:16 +0200

    Created the decision tree

--- main.cpp
+++ main.cpp
@@ -12,24 +12,22 @@
 #include <iostream>
 using namespace std;
 
-
 DigitalOut led(LED1);
 
 /* Defines */
 #define BUFFER_SIZE 200
 
-
 /*Declaration */
-
 void checkData();
 void controlServo(float pGyroDataXYZ[3]);
 void read_magnetometer();
 void read_gyro_accelerometer();
+int decisionTree(double max, double min, double standardDev);
 
 /* Global variables declaration */
 int16_t pDataXYZ[3] = {0};
 float pGyroDataXYZ[3] = {0};
-    
+
 //magnetometer buffer
 MyCircularBuffer<int16_t, BUFFER_SIZE> magnoBuf;
 
@@ -45,25 +43,19 @@ PwmOut _pwm(D0);
 /**
  * Main function. Initialize the sensors and set up the event queue
  */
-int main() {
+int main()
+{
 
     //Initiate the sensors
     //BSP_MAGNETO_Init();
     BSP_GYRO_Init();
     //BSP_ACCELERO_Init();
 
-    //Configure eventqueue 
+    // Configure eventqueue
     EventQueue queue;
-    //queue.call_every(2000, checkData);
     //queue.call_every(25, read_magnetometer);
     queue.call_every(5, read_gyro_accelerometer);
     queue.dispatch(-1);
-}
-
-/**
- * Check the buffers and decide if we have an attack
- */ 
-void checkData() {
 }
 
 /**
@@ -71,34 +63,64 @@ void checkData() {
  * and use it to control the servo engine
  * @param pGyroDataXYZ array of the data from the gyro. On 3 axes
  */
-void controlServo(float pGyroDataXYZ[3]){
-     float xGyro = pGyroDataXYZ[0];
-     _pwm.pulsewidth_us(1500+xGyro/1000.0);
+void controlServo(float pGyroDataXYZ[3])
+{
+    float xGyro = pGyroDataXYZ[0];
+    _pwm.pulsewidth_us(1500 + xGyro / 1000.0);
 }
 
 /**
  * Read the data from the magnetometer
  */
-void read_magnetometer(){
+void read_magnetometer()
+{
     BSP_MAGNETO_GetXYZ(pDataXYZ);
-    magnoBuf.push(sqrt(pow(pDataXYZ[0],2) + pow(pDataXYZ[1],2) + pow(pDataXYZ[2],2)));
+    magnoBuf.push(sqrt(pow(pDataXYZ[0], 2) + pow(pDataXYZ[1], 2) + pow(pDataXYZ[2], 2)));
 }
 
 /**
  * Read the data from the accelerometer and from the gyro
  */
-void read_gyro_accelerometer(){
+void read_gyro_accelerometer()
+{
     BSP_GYRO_GetXYZ(pGyroDataXYZ);
     controlServo(pGyroDataXYZ);
-    //acceloBuf.push(sqrt(pow(pDataXYZ[0],2) + pow(pDataXYZ[1],2) + pow(pDataXYZ[2],2)));
 
-    if(gyroBuf.full())
+    if (gyroBuf.full())
     {
         printf("%f %f %f %f %f\n", gyroBuf.max(), gyroBuf.mean(), gyroBuf.min(), gyroBuf.standardDev(), gyroBuf.avgDev());
         gyroBuf.reset();
     }
-    gyroBuf.push(sqrt(pow(pGyroDataXYZ[0],2) + pow(pGyroDataXYZ[1],2) + pow(pGyroDataXYZ[2],2)));
-
+    gyroBuf.push(sqrt(pow(pGyroDataXYZ[0], 2) + pow(pGyroDataXYZ[1], 2) + pow(pGyroDataXYZ[2], 2)));
 }
 
-
+/**
+ * This is the implementation of the Machin Learning single sensor defence based on the readings of the gyro only.
+ * It is a simple decision tree model. It based on readings measured for 8 seconds.
+ * @param max maximum value from the gyro
+ * @param min minimum value from the gyro
+ * @param standardDev the standard deviation of the gyro readings
+ * @return 1 if there is an attack and 0 if not
+ */
+int decisionTree(double max, double min, double standardDev)
+{
+    if (max < 247406)
+    {
+        if (min < 4590.13)
+        {
+            return 0;
+        }
+        else if (standardDev < 63222.4)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
