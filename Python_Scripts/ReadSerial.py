b0a81743d4030184ec523c4e3a61a494542ff3c7
hourish <houish@post.bgu.ac.il>
2019-01-17 12:07:22 +0200

    17\01

--- Python_Scripts/ReadSerial.py
+++ Python_Scripts/ReadSerial.py
@@ -20,10 +20,12 @@ if len(sys.argv) == 3:
     csv_file = open(file, 'w', newline='')
     csv_writer = csv.writer(csv_file)
     # title = "Max Mean Min StandardDev AvgDev"
-    title = "MSE"
+    # title = "MSE"
+    title = "Not_attack Under_attack"
     lineList = [title.split()]
-    timeout = time.time() + 60*10
-
+    timeout = time.time() + 10
+    attacks = 0
+    non_attacks = 0
     ''' loop all the time and collect the data. Insert the data to csv file.'''
     while True:
         if time.time() > timeout:
@@ -31,7 +33,12 @@ if len(sys.argv) == 3:
         line = s.readline()
         line = line.decode('utf-8')
         if line != '':
-            lineList.append(line.split())
+            if line == 'No attack\n':
+                non_attacks += 1
+            elif line == 'Under attack\n':
+                attacks += 1
+    summary = str(non_attacks) + ' ' + str(attacks)
+    lineList.append(summary.split())
     csv_writer.writerows(lineList)    
 else:
     print('The number of arguments needed to be 3, the script, the serial to listen to and the path for the csv file')
